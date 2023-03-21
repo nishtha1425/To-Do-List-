7fe675ee7e963d33958b28a576035f32840e8416
nishtha1425 <119800981+nishtha1425@users.noreply.github.com>
2023-03-22 02:18:42 +0530

    add files for the ap

---
+++ app.js
@@ -0,0 +1,54 @@
+
+const express =  require("express");
+const bodyParser = require("body-parser");
+const date = require(__dirname + "/date.js");
+
+const app = express();
+
+let items = ["Buy groceries", "Complete Module", "Buy stationary"];
+let workItems = [];
+
+app.set("view engine", "ejs");
+
+app.use(bodyParser.urlencoded({extended: true}));
+
+app.use(express.static("public"));
+
+app.get("/", function(req, res){
+    
+    let day = date();
+
+        res.render("list", {
+            listTitle: day,
+            newListItem: items
+        });
+
+    
+});
+
+app.get("/work", function(req, res){
+
+    res.render("list", {
+        listTitle: "Work List",
+        newListItem: workItems
+    });
+});
+
+
+app.post("/", function(req, res){
+
+    let item = req.body.newItem;
+
+    if (req.body.list === "Work List"){
+        workItems.push(item);
+        res.redirect("/work");
+    } else{
+        items.push(item);
+        res.redirect("/");
+    };
+});
+
+
+app.listen(3000, function(){
+    console.log("server started on port 3000");
+});
